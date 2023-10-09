8571c52607bce4a5c58070b8244ea5524ae1e1b2
diegoelizalde4 <diegoelizalde475@gmail.com>
2023-10-09 17:47:09 -0600

    Send params in the URL and get them in the controller

---
+++ src/main/java/com/bolsadeideas/springboot/web/app/controller/EjemploParamsController.java
@@ -0,0 +1,25 @@
+package com.bolsadeideas.springboot.web.app.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/params")
+public class EjemploParamsController {
+
+    @GetMapping("/")
+    public String index(){
+        return "params/index";
+    }
+
+    @GetMapping("/string")
+    public String param(@RequestParam(name="texto",required=false, defaultValue="Algun valor") String texto, Model model){
+        model.addAttribute("resultado", "El texto enviado es: " + texto);
+        return "params/ver";
+    }
+
+    
+    
+}
